d52883c138adcc8d742e74dfce373518da70578a
akihide-hatano <a.h.19841202@gmail.com>
2025-03-02 16:20:59 +0900

    html,css.jsを修正しました localStorageに保存してそれを表示するようにしました

--- js/output.js
+++ js/output.js
@@ -16,10 +16,27 @@
             const nameP = document.createElement("p");
             nameP.textContent = `氏名: ${outName ?? "未設定"}`;
             outputArea.appendChild(nameP);
-            //年齢をoutputAreaに追加する。
+            //性別をoutputAreaに追加する
             const genderP = document.createElement("p");
-            genderP.textContent =`年齢:${outGender ?? "未設定"}`;
+            genderP.textContent =`性別:${outGender ?? "未設定"}`;
             outputArea.appendChild(genderP);
+            //年齢をoutputAreaに追加する
+            const ageP = document.createElement("p");
+            ageP.textContent =  `年齢:${outAge ?? "未設定"}`;
+            outputArea.appendChild(ageP);
+            //特技をoutputAreaに追加する
+            const specoalityP = document.createElement("p");
+            specoalityP.textContent =  `特技:${outSpecoality ?? "未設定"}`;
+            outputArea.appendChild(specoalityP);
+            //skillをoutputAreaに追加する
+            const skillP = document.createElement("p");
+            skillP.textContent =  `スキル:${outSkills ?? "未設定"}`;
+            outputArea.appendChild(skillP);
+            //skillをoutputAreaに追加する
+            const prP = document.createElement("p");
+            prP.textContent =  `自己pr:${outPr ?? "未設定"}`;
+            outputArea.appendChild(prP);
+
 
         } else {
             outputArea.textContent = "データが見つかりませんでした。";
